3bf1812a4ca5e532cf275320af5ec9849b1f69cd
kevinnadar <jesikamaraj@gmail.com>
2022-02-10 17:56:12 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
-API_ID = int("18413164")
-API_HASH = "ab275d22b30fc931aa3324972a95f675"
-BOT_TOKEN = "5221302457:AAELQZr8pCFZMGWHwQ2Lq07tojS2oXfzsac"
-MODE = "mdisk"
-DROPLINK_KEY = "5123699a9d8d535b0065e0e7908e311551527a68"
-MDISK_KEY = "6LZq851sXoPHuwqgiKQq"
-INCLUDE_DOMAIN = ""
-EXCLUDE_DOMAIN = ""
-CHANNELS = bool(os.environ.get("CHANNELS"))
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+MODE = os.environ.get("MODE")
+DROPLINK_KEY = os.environ.get("DROPLINK_KEY")
+MDISK_KEY = os.environ.get("MDISK_KEY")
+INCLUDE_DOMAIN = os.environ.get("INCLUDE_DOMAIN")
+EXCLUDE_DOMAIN = os.environ.get("EXCLUDE_DOMAIN")
 CHANNEL_ID = list(int(i) for i in os.environ.get("CHANNEL_ID").split(" ")) if os.environ.get("CHANNEL_ID") else []
 FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
 ADMINS = list(int(i) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
-SOURCE_CODE = ""
+SOURCE_CODE = "https://github.com/kevinnadar22/URL-Shortener-V2/"
+
 
