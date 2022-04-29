62f56ea481e320f00d723efcb105340e30221014
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-04-29 17:24:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,34 +1,18 @@
-# import os
-#
-# API_ID = int(os.environ.get("API_ID"))
-# API_HASH = os.environ.get("API_HASH")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN")
-# METHOD = os.environ.get("METHOD")
-# DROPLINK_API = os.environ.get("DROPLINK_API")
-# MDISK_API = os.environ.get("MDISK_API")
-# INCLUDE_DOMAIN = os.environ.get("INCLUDE_DOMAIN")
-# EXCLUDE_DOMAIN = os.environ.get("EXCLUDE_DOMAIN")
-# CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")) if os.environ.get("CHANNEL_ID") else []
-# FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
-# ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
-# SOURCE_CODE = "https://github.com/kevinnadar22/URL-Shortener-V2"
-# CHANNELS = bool(os.environ.get("CHANNELS"))
-# USERNAME = os.environ.get("USERNAME")
-
-
 import os
 
-API_ID = 12405606
-API_HASH = "705f624d87506fb1f5b88dae01d738f7"
-BOT_TOKEN = "5380604595:AAGQenmdCFL8VAJOYTCmOe0R62ASrGwdOmA"
-METHOD = "mdisk"
-DROPLINK_API = "1aab74171e9891abd0ba799e3fd568c9598a79e1"
-MDISK_API = "6LZq851sXoPHuwqgiKQq"
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+METHOD = os.environ.get("METHOD")
+DROPLINK_API = os.environ.get("DROPLINK_API")
+MDISK_API = os.environ.get("MDISK_API")
 INCLUDE_DOMAIN = os.environ.get("INCLUDE_DOMAIN")
 EXCLUDE_DOMAIN = os.environ.get("EXCLUDE_DOMAIN")
-CHANNEL_ID = list(int(i.strip()) for i in "-1001635837706".split(" ")) if "-1001635837706" else []
-FORWARD_MESSAGE = True
-ADMINS = list(int(i.strip()) for i in "1510060563".split(",")) if "1510060563" else []
+CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")) if os.environ.get("CHANNEL_ID") else []
+FORWARD_MESSAGE = bool(os.environ.get("FORWARD_MESSAGE"))
+ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
 SOURCE_CODE = "https://github.com/kevinnadar22/URL-Shortener-V2"
-CHANNELS = True
-USERNAME = "T2linkss"
+CHANNELS = bool(os.environ.get("CHANNELS"))
+USERNAME = os.environ.get("USERNAME")
+
+
