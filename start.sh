5a44c3dc0963e1137ca9d4496169bca83c6408b1
kevinnadar <jesikamaraj@gmail.com>
2022-07-13 12:24:17 +0530

    .

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $SOURCE_CODE ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+else
+  echo "Cloning Custom Repo from $SOURCE_CODE "
+  git clone $SOURCE_CODE /URL-Shortener-V2
+fi
+cd /URL-Shortener-V2
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 -m main
