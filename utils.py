24295c0fe03fd836c44de9713682e4dea4a7c79a
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2022-07-13 11:04:53 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -212,8 +212,13 @@ async def replace_username(text):
 	if USERNAME:
 		usernames = re.findall("([@#][A-Za-z0-9_]+)", text)
 		for i in usernames:
-			text = text.replace(i, f"@{USERNAME}")
-	return text
+			text = text.replace(i, f"@{USERNAME}
+		telegram_links = re.findall(r'[(?:http|https)?://]*(?:t.me|telegram.me|telegram.dog|youtu.be|instagram.com|youtube.com|s.channelcom.tech)[^\s]+', text)
+
+    for i in telegram_links:
+        text = text.replace(i, f"@{USERNAME}")
+
+    return text	
 	
 
 #####################  Extract all urls in a string ####################
