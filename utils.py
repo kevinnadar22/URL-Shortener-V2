206778de96a6349c985fbc9177181d9b53cfeaf8
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2022-07-13 11:19:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -212,7 +212,7 @@ async def replace_username(text):
 	if USERNAME:
 		usernames = re.findall("([@#][A-Za-z0-9_]+)", text)
 		for i in usernames:
-			text = text.replace(i, f"@{USERNAME}
+			text = text.replace(i, f"@{USERNAME}")
 		telegram_links = re.findall(r'[(?:http|https)?://]*(?:t.me|telegram.me|telegram.dog|youtu.be|instagram.com|youtube.com|s.channelcom.tech)[^\s]+', text)
 
     for i in telegram_links:
