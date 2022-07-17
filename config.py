1940a4b64ac49f0bd4eefd9fabeede89cd86e129
kevinnadar <jesikamaraj@gmail.com>
2022-07-17 13:55:47 +0530

    testing

--- config.py
+++ config.py
@@ -17,6 +17,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 INCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("INCLUDE_DOMAIN").split(",")) if os.environ.get("INCLUDE_DOMAIN") else []
 EXCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("EXCLUDE_DOMAIN").split(",")) if os.environ.get("EXCLUDE_DOMAIN") else []
 CHANNELS = True if os.environ.get("CHANNELS") == "True" else False
+print(type(os.environ.get("CHANNELS")))
+print(os.environ.get("CHANNELS"))
 CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")) if os.environ.get("CHANNEL_ID") else []
 FORWARD_MESSAGE = True if os.environ.get("FORWARD_MESSAGE") == "True" else False
 SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://github.com/kevinnadar22/URL-Shortener-V2")
@@ -26,3 +28,4 @@ FOOTER_TEXT = os.environ.get("FOOTER_TEXT", '')
 BANNER_IMAGE = os.environ.get("BANNER_IMAGE", '')
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '')
 LINK_BYPASS = True if os.environ.get("LINK_BYPASS") == "True" else False
+
