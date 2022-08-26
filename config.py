4dc417273fddc132f5c4a4bb5b0236b9464db1c0
kevinnadar <jesikamaraj@gmail.com>
2022-08-26 14:10:13 +0530

    🥲

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ FORWARD_MESSAGE = is_enabled((os.environ.get('FORWARD_MESSAGE', "False")), False
 #  Heroku Config for Dynos stats
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None) # your heroku account api from https://dashboard.heroku.com/account/applications
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None) # your heroku app name
-HEROKU = True if HEROKU_API_KEY and HEROKU_APP_NAME and ".heroku" in os.environ("PYTHONHOME", "") else False # True or False
+HEROKU = True if HEROKU_API_KEY and HEROKU_APP_NAME and ".heroku" in os.environ.get("PYTHONHOME", "") else False # True or False
 
 #  Replit Config for Hosting in Replit
 REPLIT_USERNAME = os.environ.get("REPLIT_USERNAME", None) # your replit username 
