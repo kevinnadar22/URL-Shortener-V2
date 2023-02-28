4cdaa8010fc91ba618fa46c41647cdf16ebd75eb
kevinnadar <jesikamaraj@gmail.com>
2023-02-28 22:49:01 +0530

    bug fixes

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM python:3.10-slim-buster
+
 RUN apt update && apt upgrade -y
-RUN apt-get install build-essential python3-dev -y
 RUN apt install git -y
+RUN apt-get install build-essential python3-dev -y
+COPY requirements.txt /requirements.txt
+
 RUN cd /
-RUN mkdir /URL-Shortener-V2 
-WORKDIR /URL-Shortener-V2 
+RUN pip3 install -U pip && pip3 install -U -r requirements.txt
+RUN mkdir /URL-Shortener-V2
+WORKDIR /URL-Shortener-V2
 COPY start.sh /start.sh
 CMD ["/bin/bash", "/start.sh"]
--- start.sh
+++ start.sh
@@ -1,20 +1,12 @@
-if [ -d "/URL-Shortener-V2" ]
-then
-  echo "Deleting existing directory..."
-  rm -rf /URL-Shortener-V2
-fi
-
-if [ -z $SOURCE_CODE ]
+if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
   git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
 else
-  echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /URL-Shortener-V2
 fi
 cd /URL-Shortener-V2
-python3 -m venv venv
-source venv/bin/activate
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-python3 -m main
+python3 bot.py
