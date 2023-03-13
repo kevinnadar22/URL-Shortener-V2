18015cff4aa6cf77449b87af3110d3ae925d1fb7
kevinnadar <jesikamaraj@gmail.com>
2023-03-13 23:14:36 +0530

    ......//

--- plugins/channel.py
+++ plugins/channel.py
@@ -9,6 +9,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 # Channel
+
+
 @Client.on_message(
     ~filters.forwarded
     & filters.chat(CHANNEL_ID)
@@ -24,7 +26,7 @@ async def channel_link_handler(c: Client, message):
         vld = await user_api_check(user)
         if vld is not True and CHANNELS:
             return await c.send_message(OWNER_ID, f"To use me in channel...{vld}")
-    
+
         try:
             await main_convertor_handler(message, True, user=user)
             await update_stats(message, user_method)
@@ -35,7 +37,7 @@ async def channel_link_handler(c: Client, message):
 @Client.on_message(filters.command("test") & filters.user(OWNER_ID))
 async def test(c: Client, message):
     m_id = 3012
-    c_id = -1001769986368
+    c_id = 0
     message = await c.get_messages(c_id, m_id)
     user = await get_user(OWNER_ID)
     await main_convertor_handler(message, True, user)
