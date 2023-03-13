39f6ce6f2c6ceabccdd894b40157d9ed3472a9e5
kevinnadar <jesikamaraj@gmail.com>
2023-03-13 22:35:50 +0530

    typeo fix

--- utils.py
+++ utils.py
@@ -159,6 +159,8 @@ async def create_inline_keyboard_markup(message: Message, method_func, user):
         for markup in reply_markup["inline_keyboard"]:
             row_buttons = []
             for button_data in markup:
+                if button_data.get("url") is None:
+                    continue
                 text = button_data["text"]
                 url = await method_func(user=user, text=button_data["url"])
                 row_buttons.append(InlineKeyboardButton(text, url=url))
